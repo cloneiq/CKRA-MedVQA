d7d24f712fd5f47b1a5b50d61d0b67305fba3d99
Lijun Liu <96814076+cloneiq@users.noreply.github.com>
2025-09-08 16:38:09 +0800

    Update ckra_test.sh

--- run_scripts/ckra_test.sh
+++ run_scripts/ckra_test.sh
@@ -7,7 +7,7 @@ python main.py with data_root=data/finetune_arrows/ \
  task_finetune_vqa_vqa_rad \
  per_gpu_batchsize=${per_gpu_batchsize} \
  clip16 text_roberta \
- image_size=384 \
+ image_size=224 \
  test_only=True \
  tokenizer=downloaded/roberta-base \
  load_path=checkpoints/VQARAD_78.27.ckpt
@@ -18,7 +18,7 @@ python main.py with data_root=data/finetune_arrows/ \
  task_finetune_vqa_slack \
  per_gpu_batchsize=${per_gpu_batchsize} \
  clip16 text_roberta \
- image_size=384 \
+ image_size=224 \
  test_only=True \
  tokenizer=downloaded/roberta-base \
  load_path=checkpoints/SLAK_82.80.ckpt
